64bdd9e9fbc668da6d717e5286ac0ab8ce479691
Kim.McK <k.mckeever@alumni.unc.edu>
2019-05-18 17:48:57 -0400

    starting work on the js app

--- Instructions/StarterCode/Belly_Button_Biodiversity/static/js/belly_app.js
+++ Instructions/StarterCode/Belly_Button_Biodiversity/static/js/belly_app.js
@@ -1,7 +1,16 @@
 function buildMetadata(sample) {
 
   // @TODO: Complete the following function that builds the metadata panel
-
+  var url =`/metadata/${sample}`
+  var paneldiv=d3.select('#sample-metadata')
+  paneldiv.html("")
+  var tbody=paneldiv.append('tbody')
+  d3.json(url).then(function(data){
+    Object.entries(data).forEach(([key,value])=>{
+      var row=tbody.append('tr')
+      row.append('td').text(`${key}: ${value}`)
+      row.append('td').text('')
+    })
   // Use `d3.json` to fetch the metadata for a sample
     // Use d3 to select the panel with id of `#sample-metadata`
 
