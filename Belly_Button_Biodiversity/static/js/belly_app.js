3f9ba3c1412506ef7691070aed01f34e6ca7b384
Kim.McK <k.mckeever@alumni.unc.edu>
2019-05-28 23:53:28 -0400

    push

--- Belly_Button_Biodiversity/static/js/belly_app.js
+++ Belly_Button_Biodiversity/static/js/belly_app.js
@@ -57,10 +57,42 @@ function buildChart(sample) {
         name: 'frequency',
         text: level,
         hoverinfo: 'text+name'},
-        {
-          /// !!! ///
-        }
-      }]
+        { values: [50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50],
+          rotation: 90,
+          text: ['8-9','7-8','6-7', '5-6', '4-5', '3-4','2-3', '1-2', '0-1'],
+          textinfo: 'text',
+          textposition:'inside',
+          marker: {colors:['rgba(0, 128, 0, 1)', 'rgba(0, 128, 0, .9)',
+                                'rgba(0, 128, 0, .8)', 'rgba(0, 128, 0, .7)',
+                                'rgba(0, 128, 0, .6)', 'rgba(0, 128, 0, .5)',
+                                'rgba(0, 128, 0, .4)', 'rgba(0, 128, 0, .3)',
+                                'rgba(0, 128, 0, .2)',
+                                'rgba(255, 255, 255, 0)']},
+          labels: ['Obsessively Squeaky clean...','Extra Squeaky Clean!','Showers Daily','Showers Regularly','Showers Regularly','Showers Ocassionally','Low','A Bit Dusty','Full of Lint'],
+          hoverinfo: 'label',
+          hole: .5,
+          type: 'pie',
+          showlegend: false
+        }];
+        var layout = {
+          shapes:[{
+          type: 'path',
+          path: path,
+          fillcolor: '850000',
+          line: {
+            color: '850000'
+          }
+        }],
+      title: '<b>Belly Button Washing</b> <br> Scrubs per Week',
+      xaxis: {zeroline:false, showticklabels:false,
+                showgrid: false, range: [-1, 1]},
+      yaxis: {zeroline:false, showticklabels:false,
+                showgrid: false, range: [-1, 1]}
+    };
+
+    Plotly.newPlot('gauge', data, layout);
+
+
 
 
 /// Pie Chart ///      
@@ -95,7 +127,7 @@ function init() {
   });
 }
 
-function sampleChanged(newSample) {
+function optionChanged(newSample) {
   buildChart(newSample);
   dataMetadata(newSample);
 }
